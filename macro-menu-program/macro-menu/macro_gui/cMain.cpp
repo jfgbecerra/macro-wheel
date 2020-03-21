97800f78306b95de4f3f222cbaacde57041a6726
Coleman Alexander Cook <coleman.a.cook@gmail.com>
2020-03-21 18:23:20 -0500

    made some definitely appropriate changes to the gui for testing

---
+++ macro-menu-program/macro-menu/macro_gui/cMain.cpp
@@ -0,0 +1,28 @@
+#include "cMain.h"
+
+wxBEGIN_EVENT_TABLE(cMain, wxFrame)
+	EVT_BUTTON(10001, OnButtonClicked)
+
+wxEND_EVENT_TABLE()
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "MacroPenis")
+{
+	m_btn1 = new wxButton(this, 10001, "touch me daddy", wxPoint(10, 10), wxSize(250, 50));;
+	m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
+	m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
+
+	
+
+	
+}
+
+cMain::~cMain()
+{
+
+}
+
+void cMain::OnButtonClicked(wxCommandEvent& evt)
+{
+	m_list1->AppendString(m_txt1->GetValue());
+	evt.Skip();
+}
---
+++ macro-menu-program/macro-menu/macro_gui/cMain.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "wx/wx.h"
+
+class cMain : public wxFrame
+{
+public:
+	cMain();
+	~cMain();
+
+public:
+	wxButton* m_btn1 = nullptr;
+	wxTextCtrl* m_txt1 = nullptr;
+	wxListBox* m_list1 = nullptr;
+
+	void OnButtonClicked(wxCommandEvent& evt);
+
+	wxDECLARE_EVENT_TABLE();
+};
+
