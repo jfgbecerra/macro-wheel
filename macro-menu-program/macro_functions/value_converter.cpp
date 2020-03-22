d9b73841a77421637fe60e404a2fc2019153e6e6
Jose Garcia Becerra <jfb0009@uah.edu>
2020-03-21 21:17:47 -0500

    Added first function.

---
+++ macro-menu-program/macro_functions/value_converter.cpp
@@ -0,0 +1,14 @@
+// Driver script to call your functions that you want to run for macros this is a call for a python script 
+// You can make calls for other languages like this. 
+
+#include <iostream>
+#include<iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+
+    int result = system("python val_converter.py");
+    system("pause");
+    return 0;
+}
