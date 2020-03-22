3f81f39468fdb8aa9659b6a1b41d74f9d8b96bc3
Coleman Alexander Cook <coleman.a.cook@gmail.com>
2020-03-21 21:10:58 -0500

    GUI changes to make the application less memey and to implement exit function. Functionality can now be added to each macro key.

--- macro-menu-program/cMain.h
+++ macro-menu-program/cMain.h
@@ -8,17 +8,20 @@ public:
 	~cMain();
 
 public:
+
+	// *** example declarations for application objects ***
 	//wxButton* m_btn1 = nullptr;
 	//wxTextCtrl* m_txt1 = nullptr;
 	//wxListBox* m_list1 = nullptr;
 
-	// the fields for height and width of button grid
+	// Fields for the macro button grid dimensions
 	int nFieldWidth = 3;
 	int nFieldHeight = 3;
 
 	wxButton** btn;
 
 	void OnButtonClicked(wxCommandEvent& evt);
+
 	void HotKeyDown(wxKeyEvent& evt);
 
 	wxDECLARE_EVENT_TABLE();
